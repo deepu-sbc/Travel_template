566088566dd6dae68f09c018d32858abac03dfcf
deepu-sbc <saideepikha1501@gmail.com>
2024-10-09 20:08:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+<script>
+    const hamburg = document.querySelector(".hamburg")
+    const cancel = document.querySelector(".cancel")
+    const cancel = document.querySelector(".dropdown")
+
+
+</script>
